37103bdf2e9c480a0af009fe9fc4415cec37e332
Rouyu Chen <u6539353@anu.edu.au>
2021-03-17 14:15:27 +1100

    Build question form

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -5,7 +5,16 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">All Questions</div>
+                <div class="card-header">
+                    <div class="d-flex align-items-center">
+                        <h2>All Questions</h2>
+                        <div class="ml-auto">
+                            {{-- If we click the button, we will go to the create page --}}
+                            <a href="{{ route('questions.create') }}" class="btn btn-outline-secondary">Ask Question</a>
+                            {{-- Then we need to fill up the create method --}}
+                        </div>
+                    </div>
+                </div>
                 <div class="card-body">
                     @foreach ($questions as $question)
                         <div class="media">
