bedbcb577076e9f253865526830545cab89de6c4
Rouyu Chen <u6539353@anu.edu.au>
2021-04-04 16:08:19 +1000

    Create VotableTableSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UsersQuestionsAnswersTableSeeder::class,
             FavoritesTableSeeder::class,
+            VotablesTableSeeder::class,
         ]);
         // \App\Models\User::factory(10)->create();
     }
